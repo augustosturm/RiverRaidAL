3188afc30c60b6de22a36b14ac042e34e0d21c8e
leandroMarcolinF <leandromarcolin3@gmail.com>
2025-10-23 18:50:41 -0300

    Refatorado movimentação do inimigo para maior clareza de código

--- Entities/enemy.c
+++ Entities/enemy.c
@@ -4,6 +4,10 @@
 void moveEnemy(Enemy *e) {
     e->entity.x += e->speed * e->direction * GetFrameTime();
 
+    changeEnemyDirection(e);
+}
+
+void changeEnemyDirection(Enemy *e) {
     if (e->entity.x > e->maxX || e->entity.x < e->minX) {
         e->direction *= -1;
     }
